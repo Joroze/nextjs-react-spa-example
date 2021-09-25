84c89d46e450d8d95dcec6360ae1f88309c2fb1f
Jordan <tell.jordanr@gmail.com>
2021-09-25 12:35:02 -0400

    Test Auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log('hi');
+  return (
+    <Auth0Provider
+      domain="joroze.auth0.com"
+      clientId="8ftOtNWBVoOPS4yW0Yo0x1vdXy7MoG5X"
+      redirectUri="http://localhost:3000/"
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
